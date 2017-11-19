3fc735789a61363c771913f89f0a6d07e5e8693b
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-11-19 18:15:24 +0530

    Responsive size for nav icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,20 +14,35 @@ const HeaderComp = ({ activeTab }) => {
             <div className="row"><TitleBar /></div>
 
             <ul className="nav nav-tabs" role="tablist">
-                <li className={activeTab === TAB_COUPLE ? activeTabClass : defaultTabClass}><NavLink activeClassName="active" to='/couple'>
-                    <img src="img/couple.png" className="tabIcon" alt="couple"/><span className="hidden-xs">Our Story</span></NavLink></li>
-                
-                <li className={activeTab === TAB_WEDDING ? activeTabClass : defaultTabClass}><NavLink activeClassName="active" to='/wedding'>
-                    <img src="img/wedding.png" className="tabIcon" alt="wedding"/><span className="hidden-xs">The Wedding</span></NavLink></li>
+                <li className={activeTab === TAB_COUPLE ? activeTabClass : defaultTabClass}>
+                    <NavLink activeClassName="active" to='/couple'>
+                        <img src="img/couple.png" className="tabIcon" alt="couple" /><span className="hidden-xs">Our Story</span>
+                    </NavLink>
+                </li>
 
-                <li className={activeTab === TAB_PHOTOS ? activeTabClass : defaultTabClass}><NavLink activeClassName="active" to='/photos'>
-                    <img src="img/photos.png" className="tabIcon" alt="photos"/><span className="hidden-xs"> Photos</span></NavLink></li>
+                <li className={activeTab === TAB_WEDDING ? activeTabClass : defaultTabClass}>
+                    <NavLink activeClassName="active" to='/wedding'>
+                        <img src="img/wedding.png" className="tabIcon" alt="wedding" /><span className="hidden-xs">The Wedding</span>
+                    </NavLink>
+                </li>
 
-                <li className={activeTab === TAB_RSVP ? activeTabClass : defaultTabClass}><NavLink activeClassName="active" to='/rsvp'>
-                    <img src="img/rsvp.png" className="tabIcon" alt="rsvp"/><span className="hidden-xs"> RSVP</span></NavLink></li>
-                
-                <li className={activeTab === TAB_LOGISTICS ? activeTabClass : defaultTabClass}><NavLink activeClassName="active" to='/logistics'>
-                    <img src="img/logistics.png" className="tabIcon" alt="logistics"/><span className="hidden-xs">Logistics</span></NavLink></li>
+                <li className={activeTab === TAB_PHOTOS ? activeTabClass : defaultTabClass}>
+                    <NavLink activeClassName="active" to='/photos'>
+                        <img src="img/photos.png" className="tabIcon" alt="photos" /><span className="hidden-xs"> Photos</span>
+                    </NavLink>
+                </li>
+
+                <li className={activeTab === TAB_RSVP ? activeTabClass : defaultTabClass}>
+                    <NavLink activeClassName="active" to='/rsvp'>
+                        <img src="img/rsvp.png" className="tabIcon" alt="rsvp" /><span className="hidden-xs"> RSVP</span>
+                    </NavLink>
+                </li>
+
+                <li className={activeTab === TAB_LOGISTICS ? activeTabClass : defaultTabClass}>
+                    <NavLink activeClassName="active" to='/logistics'>
+                        <img src="img/logistics.png" className="tabIcon" alt="logistics" /><span className="hidden-xs">Logistics</span>
+                    </NavLink>
+                </li>
             </ul>
         </div>
     )
