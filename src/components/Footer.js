54c3be3a325dee9c2936fafe3e60a142a7238da8
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-11-23 23:57:30 +0530

    Fix footer to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,35 @@
 import React from 'react';
 
-const footerStyle = {
-    'color': 'grey',
-    'textAlign': 'center',
-    'fontFamily': 'monospace',
-    'fontSize': 'xx-small'
+const style = {
+    backgroundColor: "#F8F8F8",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    width: "100%",
+    fontFamily: "monospace",
+    fontSize: "xx-small"
+}
+
+const phantom = {
+    display: "block",
+    padding: "20px",
+    height: "60px",
+    width: "100%",
 }
 
 const Footer = () => {
     return (
-        <div style={footerStyle}>
-            Handsewn by Sush &amp; Vibe. 2017
+        <div>
+            <div style={phantom} />
+            <div style={style}>
+                Handsewn by Sush &amp; Vibe. 2017
             <a href="https://github.com/vaibhawj/por-febour-client" target="_blank"
-                rel="noopener noreferrer">
-                <img src="img/github.png" className="tabIcon" alt="photos" />
-            </a>
+                    rel="noopener noreferrer">
+                    <img src="img/github.png" className="github-icon" alt="github" />
+                </a>
+            </div>
         </div>
     )
 }
