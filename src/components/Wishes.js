3de1cf3663914d2278ae065c248d12e9177c17a7
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-12-01 01:34:15 +0530

    Remove spinner

--- src/components/Wishes.js
+++ src/components/Wishes.js
@@ -5,16 +5,6 @@ import FacebookProvider, { Comments } from 'react-facebook';
 
 const FBAppId = "134669920573096";
 
-window.onload = function () {
-    if(window.FB && window.FB.Event) {
-        window.FB.Event.subscribe('xfbml.render', function (response) {
-            document.getElementById('fb-loader').style.display = 'none';
-        });
-    } else {
-        document.getElementById('fb-loader').style.display = 'none';
-    }
-};
-
 class WishesComp extends React.Component {
 
     componentWillMount() {
@@ -24,7 +14,6 @@ class WishesComp extends React.Component {
     render() {
         return (
             <div style={{ marginLeft: '2%', marginRight: '2%' }}>
-                <span id="fb-loader" style={{marginLeft: '45%'}}><img src="img/fb-spinner.gif" className="tabIcon" alt="Loading..." /></span>
                 <FacebookProvider appId={ FBAppId }>
                     <Comments href="https://sush-and-vaibhaw.herokuapp.com/wishes" />
                 </FacebookProvider>
