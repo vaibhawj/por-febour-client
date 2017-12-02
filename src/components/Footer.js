4b91ff246186ac4dad167a0ff5a6e3764f1e30f4
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-12-02 21:25:11 +0530

    Grey footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,8 @@
 import React from 'react';
 
-const footerStyle = {
-    'color': '#99341e',
-    'textAlign': 'center',
-    'fontFamily': 'monospace',
-    'fontSize': 'xx-small'
-}
-
 const Footer = () => {
     return (
-        <div style={footerStyle}>
+        <div className="footer">
             Handsewn by Sush &amp; Vibe. 2017
             <a href="https://github.com/vaibhawj/por-febour-client" target="_blank"
                 rel="noopener noreferrer">
