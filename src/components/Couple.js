4571d1651f8d6b194912ae6ab241c2b15823a16a
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-12-04 12:26:37 +0530

    Rephrase

--- src/components/Couple.js
+++ src/components/Couple.js
@@ -18,8 +18,8 @@ class CoupleComp extends React.Component {
                     </div>
                     <div className="col-xs-12 col-sm-6">
                         <p>They met in College 10 years back and hit it off pretty fast</p>
-                        <p>Girl is nerdy 🤓 . But, she would rather boast off her Running feats 🏃🏻‍ and sports medals than her high scores. She is moody.</p>
-                        <p>Boy has always been looked upon as sincere and silent type. But, she knows the truth 😈 . He is foody.</p>
+                        <p>Girl is nerdy <span role="img" aria-label="">🤓</span>. But, she would rather boast off her Running feats <span role="img" aria-label="">🏃🏻‍</span> and sports medals than her high scores. She is moody.</p>
+                        <p>Boy has always been looked upon as sincere and silent type. But, she knows the truth that he is plain shy <span role="img" aria-label="">😈</span>. He is foody.</p>
                         <p>He tries really hard keeping up with her mood swings. In return, she gives the good company in his food swings even though she is a Calorie calculator.</p>
                     </div>
                 </div>
