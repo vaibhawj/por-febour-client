7797643775ddb329fca89ce3eaea6ec0161db42e
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2018-01-28 21:16:12 +0530

    Update venue

--- src/components/Logistics.js
+++ src/components/Logistics.js
@@ -5,7 +5,7 @@ import { ACTION_SET_ACTIVE_TAB, TAB_LOGISTICS } from '../constants';
 const iframeWidth = "100%";
 const iframeHeight = "350px";
 
-const iframeHtml = `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d124431.71832435323!2d77.56368747855326!3d12.940389910708253!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae161206c93841%3A0x182db9fe5710e140!2sTerapanth+Bhawan+Bangalore!5e0!3m2!1sen!2sin!4v1511044279372" width=${iframeWidth} height=${iframeHeight} frameborder="0" style="border:0" allowfullscreen></iframe>`;
+const iframeHtml = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d971.9778947282741!2d77.5790128292092!3d12.977506999428273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae160df938ac0f%3A0x7eb578563b8d398d!2sTerapanth+Bhawan!5e0!3m2!1sen!2sin!4v1517153288308" width=${iframeWidth} height=${iframeHeight} frameborder="0" style="border:0" allowfullscreen></iframe>`;
 
 class LogisticsComp extends React.Component {
 
@@ -25,7 +25,7 @@ class LogisticsComp extends React.Component {
     render() {
         return (
             <div style={{ marginLeft: '2%', marginRight: '2%' }} >
-                <p>We will update the detailed schedule soon. But, you have an idea how it happens <span role="img" aria-label="">😁</span></p>
+                <p>Wedding functions start at 7 pm <span role="img" aria-label="">💃🏼🕺🏽</span></p>
                 <p>Here is how you get to the place</p>
                 <div className="map-holder">
                     <div dangerouslySetInnerHTML={this.iframe()} />
